d0b040b9ed0d9858faa17a017f9e9f9fa9daabe2
Zachary Croft <croft.z@northeastern.edu>
2026-02-10 11:49:34 -0500

    leader key remap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>e", "<cmd>lua Snacks.explorer({ cwd = vim.fn.getcwd() })<CR>", { desc = "Explorer (cwd)" })
+vim.keymap.set("n", "<leader>E", "<cmd>lua Snacks.explorer()<CR>", { desc = "Explorer (root)" })
